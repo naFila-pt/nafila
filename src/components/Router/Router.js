b551439d32d5b76eb96605e3c288f9da6984ec4f
Ricardo Ferreira <me@thegiantbeast.tech>
2020-03-22 02:18:59 +0000

    Create translations entries, rename bg assets, add all the onboarding views and skip onboarding feature

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import OnBoardingContent from "../../pages/OnBoardingContent";
 import HomeContent from "../../pages/HomeContent";
 import AdminContent from "../../pages/AdminContent";
+import PrintCodeContent from "../../pages/PrintCodeContent";
 import NotFoundContent from "../../pages/NotFoundContent";
 
 class Router extends Component {
@@ -14,12 +15,17 @@ class Router extends Component {
     // Properties
     const { user } = this.props;
 
+    const shouldSkipOnBoarding = localStorage.getItem('skipOnBoarding');
+
     return (
       <BrowserRouter basename={process.env.REACT_APP_BASENAME}>
         <Switch>
           <Route path="/" exact>
-            <OnBoardingContent />
-            {/* <HomeContent user={user} /> */}
+            {shouldSkipOnBoarding ? <HomeContent user={user} /> : <OnBoardingContent />}
+          </Route>
+
+          <Route path="/admin/code">
+            <PrintCodeContent />
           </Route>
 
           <Route path="/admin">
