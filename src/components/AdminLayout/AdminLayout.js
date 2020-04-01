dfefd60b642ad6a4541cfbb5fa028188092c8288
Ricardo Ferreira <me@thegiantbeast.tech>
2020-04-01 19:39:49 +0100

    Add standard layout to admin

--- src/components/AdminLayout/AdminLayout.js
+++ src/components/AdminLayout/AdminLayout.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { Box } from "@material-ui/core";
-import styled from "styled-components";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+import MenuIcon from "@material-ui/icons/Menu";
+import { makeStyles } from "@material-ui/core/styles";
 
-import LogoImage from "../../assets/logo-mini.svg";
+import { ReactComponent as LogoMini } from "../../assets/logo-mini.svg";
 
-const Logo = styled.img`
-  padding: 50px 0;
-  display: block;
-  margin: 0 auto;
-`;
+const useStyles = makeStyles({
+  container: {
+    position: "relative",
+    minWidth: "375px",
+    height: "100%",
+    padding: 0,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "0 100%"
+  },
+  toolbar: {
+    position: "static",
+    top: 0
+  },
+  menuIcon: {
+    fontSize: "2.24em",
+    color: "rgba(0, 0, 0, .54)"
+  }
+});
 
 function Layout({ children, bg, hideToolbar }) {
+  const classes = useStyles();
   const boxProps = {
     width: 1,
     height: 1,
@@ -27,7 +45,14 @@ function Layout({ children, bg, hideToolbar }) {
 
   return (
     <Box {...boxProps}>
-      {!hideToolbar && <Logo src={LogoImage} />}
+      <Grid container>
+        <Grid container direction="column">
+          <Toolbar className={classes.toolbar}>
+            <MenuIcon className={classes.menuIcon} />
+            <LogoMini style={{ flex: 0.9 }} />
+          </Toolbar>
+        </Grid>
+      </Grid>
       {children}
     </Box>
   );
