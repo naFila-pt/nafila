03d85672bcf990020794ecc0266a7364632fd246
sardyy <tiagosardinha94@hotmail.com>
2020-06-01 17:16:17 +0100

    Fix lint

--- src/pages/Admin/Login/Login.js
+++ src/pages/Admin/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Typography, TextField, Grid } from "@material-ui/core";
+import { Typography, TextField } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 import { Link, useHistory } from "react-router-dom";
 import styled from "styled-components";
